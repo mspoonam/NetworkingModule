fc15d045ac750d6800c205630e740f86e38e2dcc
mspoonam <mspoonam.learning@gmail.com>
2023-02-26 18:31:11 -0500

    Tested to compare the url counts when load is called one time and multiple times. Based on that we fixed the error

--- NetworkingModuleTests/RemoteFeedLoaderTests.swift
+++ NetworkingModuleTests/RemoteFeedLoaderTests.swift
@@ -10,13 +10,28 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesnotRequestDataFromURL() {
         let (_, client) = makeSUT()
         
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let (sut , client) = makeSUT()
+        sut.load()
+        XCTAssertNotNil(client.requestedURLs)
     }
     
     func test_load_requestsDataFromURL() {
-        let (sut , client) = makeSUT()
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut , client) = makeSUT(url: url)
         sut.load()
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_loadTwice_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut , client) = makeSUT(url: url)
+        sut.load()
+        sut.load()
+        XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
     // MARK:- Helpers
@@ -27,9 +42,9 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
+        var requestedURLs = [URL]()
         func get(from url: URL) {
-            requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }

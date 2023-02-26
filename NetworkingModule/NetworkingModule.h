9b010fc53a0d5dbf511ef72664df172c18654106
mspoonam <mspoonam.learning@gmail.com>
2023-02-26 18:06:37 -0500

    Initial Commit

---
+++ NetworkingModule/NetworkingModule.h
@@ -0,0 +1,18 @@
+//
+//  NetworkingModule.h
+//  NetworkingModule
+//
+//  Created by Poonam Pandey on 26/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkingModule.
+FOUNDATION_EXPORT double NetworkingModuleVersionNumber;
+
+//! Project version string for NetworkingModule.
+FOUNDATION_EXPORT const unsigned char NetworkingModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingModule/PublicHeader.h>
+
+
---
+++ NetworkingModuleTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,36 @@
+//
+// Base Project
+//
+
+import XCTest
+@testable import NetworkingModule
+
+final class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesnotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let (sut , client) = makeSUT()
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
+    }
+    
+    // MARK:- Helpers
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+}
+

f38de3372a136a2cd10804bd3bd9d655e04f736c
mspoonam <mspoonam.learning@gmail.com>
2023-03-04 18:41:44 -0500

    We have tested feeditems array from json when http response is 200

--- NetworkingModuleTests/RemoteFeedLoaderTests.swift
+++ NetworkingModuleTests/RemoteFeedLoaderTests.swift
@@ -81,23 +81,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversItemsOn200HTTPResponseWithJSONItems() {
         let (sut , client) = makeSUT()
         
-        let item1 = FeedItem(id: UUID(), description: nil, location: nil, imageURL: URL(string:"http://a.com")!)
-        let item1Json = [
-            "id": item1.id.uuidString,
-            "image": item1.imageURL.absoluteString
-        ]
-        let item2 = FeedItem(id: UUID(), description: "a description", location: "a location", imageURL: URL(string: "http://b.com")!)
-        let item2Json = [
-            "id": item2.id.uuidString,
-            "description": item2.description,
-            "location": item2.location,
-            "image": item2.imageURL.absoluteString
-        ]
+        let item1 = makeItem(id: UUID(), imageURL: URL(string:"http://a.com")!)
+        let item2 = makeItem(id: UUID(), description: "a description", location: "a location", imageURL: URL(string: "http://b.com")!)
         let itemJson = [
-            "items": [item1Json, item2Json]
+            "items": [item1.json, item2.json]
         ]
-        makeItem(id: UUID(), imageURL: URL(string:"http://a.com")!)
-        expect(sut, toCompleteWith: .success([item1, item2]), when: {
+        expect(sut, toCompleteWith: .success([item1.item, item2.item]), when: {
             
             let json = try! JSONSerialization.data(withJSONObject: itemJson)
             client.complete(withStatusCode: 200, data: json)
@@ -119,11 +108,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
             "description": item.description,
             "location": item.location,
             "image": item.imageURL.absoluteString
-        ].reduce(into: [String: Any]()) { (acc, e) in
-            if let value = e.value {
-                acc[e.key] = value
-            }
-        }
+        ].compactMapValues {$0}
         
         return (item: item, json: json)
     }

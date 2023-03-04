808c1e480b92df18a0289d84cccac2d4d225bc67
mspoonam <mspoonam.learning@gmail.com>
2023-03-04 11:32:29 -0500

    Respone optional params enums with HTTP succes and failure

--- NetworkingModuleTests/RemoteFeedLoaderTests.swift
+++ NetworkingModuleTests/RemoteFeedLoaderTests.swift
@@ -81,14 +81,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-        var messages = [(url: URL, completion: (Error?, HTTPURLResponse?)->Void)]()
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+        var messages = [(url: URL, completion: (HTTPClientResult)->Void)]()
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url, completion))
             requestedURLs.append(url)
         }
         // TODO: Doubt - what completions[index](error) does
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
         
         func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -96,8 +96,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
                 url: requestedURLs[index],
                 statusCode: code,
                 httpVersion: nil,
-                headerFields: nil)
-            messages[index].completion(nil, response)
+                headerFields: nil)!
+            messages[index].completion(.success(response))
         }
     }
 }

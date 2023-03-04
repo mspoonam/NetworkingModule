58a4484d18440d5c4f6e2ca70872ce664b6fbb8a
mspoonam <mspoonam.learning@gmail.com>
2023-03-04 12:24:52 -0500

    Added Data to success , now test cases fail if the completion block was removed

--- NetworkingModuleTests/RemoteFeedLoaderTests.swift
+++ NetworkingModuleTests/RemoteFeedLoaderTests.swift
@@ -72,6 +72,22 @@ final class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut , client) = makeSUT()
+        
+        var capturedError =  [RemoteFeedLoader.Error]()
+        // Arrange
+        sut.load {
+            capturedError.append($0)
+        }
+        let invalidJSON = Data.init("invalid data".utf8)
+        // capturing values
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        // Assert
+        XCTAssertEqual(capturedError, [.invalidData])
+    }
+    
+    
     // MARK:- Helpers
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
@@ -91,13 +107,13 @@ final class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int,data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil)!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
 }

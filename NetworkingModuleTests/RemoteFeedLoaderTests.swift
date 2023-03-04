9ef41c357523a72a7ea5b7a60efa87697c061031
mspoonam <mspoonam.learning@gmail.com>
2023-03-04 11:17:06 -0500

    delivers invalid response on now 200 HTTP responses

--- NetworkingModuleTests/RemoteFeedLoaderTests.swift
+++ NetworkingModuleTests/RemoteFeedLoaderTests.swift
@@ -54,6 +54,24 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(capturedError, [.connectivity])
     }
     
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut , client) = makeSUT()
+        
+        // Arrange
+        var capturedError =  [RemoteFeedLoader.Error]()
+        
+        // Arrange
+        sut.load {
+            capturedError.append($0)
+        }
+        
+        // this is stubbing
+        client.complete(withStatusCode: 400)
+        
+        // Assert
+        XCTAssertEqual(capturedError, [.invalidData])
+    }
+    
     // MARK:- Helpers
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
@@ -63,14 +81,23 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-        var messages = [(url: URL, completion: (Error)->Void)]()
-        func get(from url: URL, completion: @escaping (Error) -> Void) {
+        var messages = [(url: URL, completion: (Error?, HTTPURLResponse?)->Void)]()
+        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
             messages.append((url, completion))
             requestedURLs.append(url)
         }
         // TODO: Doubt - what completions[index](error) does
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
+        }
+        
+        func complete(withStatusCode code: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil)
+            messages[index].completion(nil, response)
         }
     }
 }

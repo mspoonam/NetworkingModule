a35cad1b550cd7bee2137597f930d520426bd5d5
mspoonam <mspoonam.learning@gmail.com>
2023-03-05 04:31:18 -0500

    Moving files to production

---
+++ NetworkingModule/Feed API/FeedItemMapper.swift
@@ -0,0 +1,31 @@
+//	
+// Base Project 
+//
+
+import Foundation
+
+internal class FeedItemMapper {
+    static var OK_200: Int { return 200 }
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == OK_200 else { throw RemoteFeedLoader.Error.invalidData }
+        let rootItem = try JSONDecoder().decode(ItemRoot.self, from: data)
+        return rootItem.items.map {$0.ele}
+    }
+    private struct ItemRoot: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var ele: FeedItem {
+            return FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+        
+    }
+
+}
+
--- NetworkingModule/Feed API/RemoteFeedLoader.swift
+++ NetworkingModule/Feed API/RemoteFeedLoader.swift
@@ -4,15 +4,6 @@
 
 import Foundation
 
-public enum HTTPClientResult {
-    case success(Data, HTTPURLResponse)
-    case failure(Error)
-}
-
-public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
-}
-
 // Dont wanna allow subclassing
 public final class RemoteFeedLoader {
     
@@ -52,29 +43,4 @@ public final class RemoteFeedLoader {
             }
         }
     }
-}
-
-private class FeedItemMapper {
-    static var OK_200: Int { return 200 }
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else { throw RemoteFeedLoader.Error.invalidData }
-        let rootItem = try JSONDecoder().decode(ItemRoot.self, from: data)
-        return rootItem.items.map {$0.ele}
-    }
-}
-
-private struct ItemRoot: Decodable {
-    let items: [Item]
-}
-
-private struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
-    
-    var ele: FeedItem {
-        return FeedItem(id: id, description: description, location: location, imageURL: image)
-    }
-    
 }
